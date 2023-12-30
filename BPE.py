68d3db8c53bd531c23a57943a1bd65226c64a156
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-12-30 13:19:47 +0200

    add max iterations and speed up decompression

--- BPE.py
+++ BPE.py
@@ -1,5 +1,7 @@
 # from functools import reduce
 from customedDS.CustomDict import CustomDict
+import cProfile
+import pstats
 
 def reduce(function, sequence, initial):
     it = iter(sequence)
@@ -52,12 +54,12 @@ class BPE():
         replacement = self.__available_characters.pop(0)
         return replacement.to_bytes(1, byteorder="big")
 
-    def __get_original(self, b: bytes, table):
+    def __get_original(self, b: bytes, reconstruction_keys, reconstruction_values):
         """Check if the byte is in the lookup table and get the original byte before decompression"""
 
-        if (b in table.Keys()):
-            b1 = self.__get_original(table.get(b)[0].to_bytes(), table)
-            b2 = self.__get_original(table.get(b)[1].to_bytes(), table)
+        if b in reconstruction_keys:
+            b1 = self.__get_original(reconstruction_values[reconstruction_keys.index(b)][0].to_bytes(), reconstruction_keys, reconstruction_values)
+            b2 = self.__get_original(reconstruction_values[reconstruction_keys.index(b)][1].to_bytes(), reconstruction_keys, reconstruction_values)
             return b1 + b2
         return b
 
@@ -67,24 +69,27 @@ class BPE():
         byte_len = data[0]
         table_chunk = data[(-3 * byte_len):]
 
-        table = CustomDict()
+        key_bytes = []
+        value_bytes = []
         for i in range(0, 3 * byte_len, 3):
-            table.set(table_chunk[i].to_bytes(), table_chunk[i + 1].to_bytes() + table_chunk[i + 2].to_bytes())
-        return table
+            key_bytes.append(table_chunk[i].to_bytes())
+            value_bytes.append(table_chunk[i + 1].to_bytes() + table_chunk[i + 2].to_bytes())
+        return key_bytes, value_bytes
 
-    def compress(self, text: str):
+    def compress(self, text: str, iterations: int):
         """Convert text to binary to compress it"""
         self.__raw_file_data = bytearray(text, "utf-8")
-        self.__unique_file_data = reduce(lambda l, x: l.append(x) or l if x not in l else l, self.__frequencies, [])
+        self.__unique_file_data = reduce(lambda l, x: l.append(x) or l if x not in l else l, self.__raw_file_data, [])
         self.__available_characters = [c for c in range(256) if c not in self.__unique_file_data]
 
         # Placeholder for the most frequent object in the last loop
         last_pair_frequency = 0
         data_len = len(self.__raw_file_data)
         compressed_data = self.__raw_file_data
+        _iter = 0
 
         # Break if file cannot be compressed further
-        while (last_pair_frequency != 1):
+        while (last_pair_frequency != 1 or _iter < iterations):
             # Loop through the data and count frequencies
             self.__frequencies = [0] * 256 * 256
             for i in range(data_len - 1):
@@ -117,6 +122,7 @@ class BPE():
             # Replace two bytes with a single byte
             compressed_data = compressed_data.replace(highest_occuring_pair, replacement)
             data_len = len(compressed_data)
+            _iter += 1
 
         replacement_length = (len(self.__lookup_table)).to_bytes()
 
@@ -128,7 +134,6 @@ class BPE():
             # Add the lookup table as overhead at the end of the file
             for key, value in self.__lookup_table.items():
                 compressed_file.write(key + value)
-
     def decompress(self, filename: str):
         """ Decompress the file back to its original format """
         
@@ -138,19 +143,20 @@ class BPE():
         decompressed_data = b""
 
         # Use the redundant information to construct dict before decompression
-        reconstruction_dict = self.__reconstruct_dict(compressed_file_data)
-        data_len = len(compressed_file_data) - len(reconstruction_dict) * 3
+        reconstruction_keys, reconstruction_values = self.__reconstruct_dict(compressed_file_data)
+        data_len = len(compressed_file_data) - len(reconstruction_keys) * 3
 
         # Iterate through every file in the compressed file and find every byte in the lookup table recursively
         for i in range(1, data_len):
-            decompressed_data += self.__get_original(compressed_file_data[i].to_bytes(), reconstruction_dict)
+            decompressed_data += self.__get_original(compressed_file_data[i].to_bytes(), reconstruction_keys, reconstruction_values)
 
         with open(file=filename.replace(".xip", "_decompressed.xml"), mode="wb") as decompressed_file:
             decompressed_file.write(decompressed_data)
 
 xip = BPE()
 
-with open(file="/home/mahmoud/Work/Uni/DSA/Project/xmlParsiewer/test_files/generic_syntactically_correct3.xml", mode="r") as file:
+with open(file="/home/mahmoud/Work/Uni/DSA/Project/xmlParsiewer/test_files/generic_syntactically_correct2.xml", mode="r") as file:
     text = file.read()
-xip.compress(text)
-# xip.decompress("output.xip")
+xip.compress(text, 10)
+print("compressed")
+xip.decompress("output.xip")
